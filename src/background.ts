c55cb0934d1a0fee5ff928ddf33dfea9b5aa1632
pipecraft <pipecraft.net@gmail.com>
2023-03-06 23:45:02 +0800

    Start coding

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,7 @@
 export {}
-console.log("HELLO WORLD FROM BGSCRIPTS")
+console.log(
+  "HELLO WORLD FROM BGSCRIPTS 1",
+  process.env.PLASMO_PUBLIC_SITE_URL,
+  process.env.PLASMO_TARGET,
+  process.env.PLASMO_TAG
+)
