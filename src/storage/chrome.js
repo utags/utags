0fcf85b8afe0cf127e4ed1fe122bb9bfbb0411c2
pipecraft <pipecraft.net@gmail.com>
2023-03-16 01:31:07 +0800

    Fix extension storage quata issue

--- src/storage/chrome.js
+++ src/storage/chrome.js
@@ -1,6 +1,6 @@
 import { Storage } from "@plasmohq/storage"
 
-const storage = new Storage()
+const storage = new Storage({ area: "local" })
 const getValue = (key) => storage.get(key)
 const setValue = (key, value) => storage.set(key, value)
 const addValueChangeListener = (key, func) => {
