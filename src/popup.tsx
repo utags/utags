08561d5f7682bd1c6ad50640aad2e36f00be2b58
pipecraft <pipecraft.net@gmail.com>
2023-04-05 16:17:13 +0800

    Add Simple popup, options page, fix firefox doesn't support :has issues

--- src/popup.tsx
+++ src/popup.tsx
@@ -7,7 +7,25 @@ function IndexPopup() {
         padding: 16,
         width: "200px",
       }}>
-      <h1>My Extension</h1>
+      <h1>小鱼标签 (UTags)</h1>
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          listStyleType: "none",
+          padding: 16,
+        }}>
+        <li>
+          <a href="https://utags.pipecraft.net/tags/" target="_blank">
+            链接列表
+          </a>
+        </li>
+        <li>
+          <a href="https://utags.pipecraft.net/data/" target="_blank">
+            导出数据/导入数据
+          </a>
+        </li>
+      </ul>
       <footer>
         Made with ❤️ by{" "}
         <a href="https://www.pipecraft.net/" target="_blank">
