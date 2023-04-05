bdd8278d1d79e17232824d422443b657000d3e82
pipecraft <pipecraft.net@gmail.com>
2023-04-05 16:23:51 +0800

    0.1.2

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -5,7 +5,7 @@ import {
 } from "browser-extension-storage"
 import { isUrl, uniq } from "browser-extension-utils"
 
-const extensionVersion = "0.1.0"
+const extensionVersion = "0.1.2"
 const databaseVersion = 2
 const STORAGE_KEY = "extension.utags.urlmap"
 let cachedUrlMap
