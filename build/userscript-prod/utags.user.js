0a411cdcd46410240791e69b85e817577e0624dc
pipecraft <pipecraft.net@gmail.com>
2023-04-05 13:29:51 +0800

    First commit of utags

---
+++ build/userscript-prod/utags.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name                 🏷️ UTags - Add usertags to links
+// @name:zh-CN           🏷️ 小鱼标签 (UTags) - 为链接添加用户标签
+// @namespace            https://utags.pipecraft.net/
+// @homepageURL          https://github.com/utags/utags#readme
+// @supportURL           https://github.com/utags/utags/issues
+// @version              0.1.0
+// @description          Allow users to add custom tags to links.
+// @description:zh-CN    此插件允许用户为网站的链接添加自定义标签。比如，可以给论坛的用户或帖子添加标签。
+// @icon                 https://utags.pipecraft.net/favicon.png
+// @author               Pipecraft
+// @license              MIT
+// @match                https://*/*
+// @match                http://*/*
+// ==/UserScript==
+//
+;(() => {
+  "use strict"
+})()
