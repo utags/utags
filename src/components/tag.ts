9c91c60e4ed6dac1f04d33fbed27d76b936ed795
pipecraft <pipecraft.net@gmail.com>
2023-04-06 10:03:15 +0800

    Fix types

--- src/components/tag.ts
+++ src/components/tag.ts
@@ -1,10 +1,13 @@
 import { createElement } from "browser-extension-utils"
 
-export default function createTag(tagName) {
+export default function createTag(tagName: string) {
   const a = createElement("a")
   a.textContent = tagName
   a.dataset.utags_tag = tagName
-  a.setAttribute("href", "https://utags.pipecraft.net/tags/#" + tagName)
+  a.setAttribute(
+    "href",
+    "https://utags.pipecraft.net/tags/#" + encodeURIComponent(tagName)
+  )
   a.setAttribute("target", "_blank")
   a.setAttribute("class", "utags_text_tag")
   return a
