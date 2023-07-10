6755c9c77a13a3bcb1451ccc81131a8005c6db56
pipecraft <pipecraft.net@gmail.com>
2023-07-10 17:28:00 +0800

    0.1.10

--- build/userscript-prod/utags.user.js
+++ build/userscript-prod/utags.user.js
@@ -4,14 +4,15 @@
 // @namespace            https://utags.pipecraft.net/
 // @homepageURL          https://github.com/utags/utags#readme
 // @supportURL           https://github.com/utags/utags/issues
-// @version              0.1.6
+// @version              0.1.10
 // @description          Allow users to add custom tags to links.
 // @description:zh-CN    此插件允许用户为网站的链接添加自定义标签。比如，可以给论坛的用户或帖子添加标签。
 // @icon                 https://utags.pipecraft.net/favicon.png
 // @author               Pipecraft
 // @license              MIT
-// @match                https://*/*
-// @match                http://*/*
+// @match                https://utags.pipecraft.net/*
+// @match                https://*.v2ex.com/*
+// @match                https://v2hot.pipecraft.net/*
 // @grant                GM.getValue
 // @grant                GM.setValue
 // @grant                GM_addValueChangeListener
