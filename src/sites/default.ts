d90c611e9a88154f47875cba7012758b404672a7
pipecraft <pipecraft.net@gmail.com>
2023-07-19 02:22:15 +0800

    feat: apply utags to hacker news(news.ycombinator.com)

--- src/sites/default.ts
+++ src/sites/default.ts
@@ -2,7 +2,7 @@ export const getCanonicalUrl = (url: string) => url
 
 const site = {
   matches: /.*/,
-  includeSelectors: ["a[href]:not(.utags_text_tag)"],
+  matchedNodesSelectors: ["a[href]:not(.utags_text_tag)"],
   excludeSelectors: [".browser_extension_settings_container"],
   getCanonicalUrl,
 }
