4a696dd85a2fbbe2ee13f932068225b4b9abc95e
pipecraft <pipecraft.net@gmail.com>
2023-07-24 14:49:35 +0800

    fix: update style

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -40,6 +40,7 @@ export async function getCachedUrlMap(): Promise<Record<string, unknown>> {
 }
 
 export function getTags(key: string): Record<string, unknown> {
+  // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
   return (cachedUrlMap && cachedUrlMap[key]) || { tags: [] }
 }
 
