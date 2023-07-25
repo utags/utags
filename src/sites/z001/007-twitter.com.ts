2593ffc95c0ef0fb48896b090321b570109031a0
pipecraft <pipecraft.net@gmail.com>
2023-07-25 13:10:40 +0800

    fix: update twitter selectors

--- src/sites/z001/007-twitter.com.ts
+++ src/sites/z001/007-twitter.com.ts
@@ -2,14 +2,16 @@ import { $$ } from "browser-extension-utils"
 
 import defaultSite from "../default"
 
+const prefix = "https://twitter.com/"
+
 const site = {
   matches: /twitter\.com/,
   getMatchedNodes() {
     return $$("a[href]:not(.utags_text_tag)").filter(
       (element: HTMLAnchorElement) => {
         const href = element.href
-        if (href.startsWith("https://twitter.com/")) {
-          // Remove "https://twitter.com/"
+        if (href.startsWith(prefix)) {
+          // Remove prefix
           const href2 = href.slice(20)
           if (/^\w+$/.test(href2)) {
             if (
@@ -36,6 +38,21 @@ const site = {
     ) as HTMLAnchorElement[]
   },
   excludeSelectors: [...defaultSite.excludeSelectors],
+  addExtraMatchedNodes(matchedNodesSet: Set<HTMLElement>) {
+    // profile header
+    const elements = $$('[data-testid="UserName"] span')
+    for (const element of elements) {
+      const title = element.textContent!.trim()
+      if (!title || !title.startsWith("@")) {
+        continue
+      }
+
+      const key = prefix + title.slice(1)
+      const meta = { title, type: "user" }
+      element.utags = { key, meta }
+      matchedNodesSet.add(element)
+    }
+  },
 }
 
 export default site
