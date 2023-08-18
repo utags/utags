1c17a36ee788267c14b1e00d0c8a1221751bd25c
pipecraft <pipecraft.net@gmail.com>
2023-08-18 10:34:09 +0800

    0.8.1

--- build/userscript-prod/utags.user.js
+++ build/userscript-prod/utags.user.js
@@ -4,7 +4,7 @@
 // @namespace            https://utags.pipecraft.net/
 // @homepageURL          https://github.com/utags/utags#readme
 // @supportURL           https://github.com/utags/utags/issues
-// @version              0.8.0
+// @version              0.8.1
 // @description          Allow users to add custom tags to links.
 // @description:zh-CN    此插件允许用户为网站的链接添加自定义标签。比如，可以给论坛的用户或帖子添加标签。
 // @icon                 data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='%23ff6361' class='bi bi-tags-fill' viewBox='0 0 16 16'%3E %3Cpath d='M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z'/%3E %3Cpath d='M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z'/%3E %3C/svg%3E
@@ -2447,6 +2447,76 @@
     },
   }
   var juejin_cn_default = site17
+  var prefix7 = "https://e-hentai.org/"
+  function getPostUrl(url) {
+    if (url.startsWith(prefix7)) {
+      const href2 = url.slice(21)
+      if (/^g\/\w+/.test(href2)) {
+        return prefix7 + href2.replace(/^(g\/\w+\/\w+\/).*/, "$1")
+      }
+    }
+    return void 0
+  }
+  var site18 = {
+    matches: /e-hentai\.org/,
+    getMatchedNodes() {
+      return $$("a[href]:not(.utags_text_tag)")
+        .filter((element) => {
+          const href = element.href
+          if (href.startsWith(prefix7)) {
+            const key = getPostUrl(href)
+            if (key) {
+              const titleElement = $(".glink", element)
+              let title
+              if (titleElement) {
+                title = titleElement.textContent
+              }
+              const meta = { type: "post" }
+              if (title) {
+                meta.title = title
+              }
+              element.utags = { key, meta }
+              return true
+            }
+          }
+          return true
+        })
+        .map((element) => {
+          const titleElement = $(".gl4e.glname .glink", element)
+          if (titleElement) {
+            titleElement.utags = element.utags
+            titleElement.dataset.utags = titleElement.dataset.utags || ""
+            titleElement.dataset.utags_node_type = "link"
+            console.log(titleElement.utags)
+            return titleElement
+          }
+          return element
+        })
+    },
+    excludeSelectors: [
+      ...default_default.excludeSelectors,
+      "#nb",
+      ".searchnav",
+      ".gtb",
+      'a[href*="report=select"]',
+      'a[href*="act=expunge"]',
+    ],
+    addExtraMatchedNodes(matchedNodesSet) {
+      const key = getPostUrl(location.href)
+      if (key) {
+        const element = getFirstHeadElement()
+        if (element) {
+          const title = element.textContent.trim()
+          if (title) {
+            const meta = { title, type: "post" }
+            element.utags = { key, meta }
+            matchedNodesSet.add(element)
+          }
+        }
+      }
+    },
+  }
+  var e_hentai_org_default = site18
   var sites = [
     github_com_default,
     v2ex_default,
@@ -2464,6 +2534,7 @@
     tiktok_com_default,
     pojie_cn_default2,
     juejin_cn_default,
+    e_hentai_org_default,
   ]
   function matchedSite(hostname2) {
     for (const s of sites) {
