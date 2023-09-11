b4e989761031bb352b179d8afc10beb84fc51fe5
pipecraft <pipecraft.net@gmail.com>
2023-09-11 09:54:06 +0800

    fix: use createHTML

--- src/content.ts
+++ src/content.ts
@@ -6,6 +6,7 @@ import {
   addEventListener,
   addStyle,
   createElement,
+  createHTML,
   doc,
   getAttribute,
   hasClass,
@@ -151,7 +152,7 @@ function appendTagsToPage(
 <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z"/>
 </svg>
 `
-  a.innerHTML = svg
+  a.innerHTML = createHTML(svg)
 
   li.append(a)
   ul.append(li)
