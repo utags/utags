816117fa4b8c56c635f4fa40c0e9123363eede79
pipecraft <pipecraft.net@gmail.com>
2023-10-24 14:03:05 +0800

    fix(github): update selectors and style (#13)

--- src/sites/z001/005-github.com.ts
+++ src/sites/z001/005-github.com.ts
@@ -1,4 +1,5 @@
 import { $$ } from "browser-extension-utils"
+import styleText from "data-text:./005-github.com.scss"
 
 import defaultSite from "../default"
 
@@ -24,7 +25,71 @@ const noneUsers = new Set([
   "sitemap",
   "git-guides",
   "open-source",
+  "marketplace",
+  "codespaces",
+  "issues",
+  "pulls",
+  "discussions",
+  "dashboard",
+  "account",
+  "new",
+  "notifications",
+  "settings",
+  "feedback",
+  "organizations",
+  "github-copilot",
+  "search",
 ])
+
+const prefix = "https://github.com/"
+
+function getUserProfileUrl(href: string) {
+  if (href.startsWith(prefix)) {
+    const href2 = href.slice(19)
+
+    let username = ""
+    if (/^[\w-]+$/.test(href2)) {
+      username = /^([\w-]+)$/.exec(href2)![1]
+    }
+
+    if (/(author%3A|author=)[\w-]+/.test(href2)) {
+      username = /(author%3A|author=)([\w-]+)/.exec(href2)![2]
+    }
+
+    if (username && !noneUsers.has(username)) {
+      return prefix + username
+    }
+  }
+
+  return undefined
+}
+
+function getRepoUrl(href: string) {
+  if (href.startsWith(prefix)) {
+    const href2 = href.slice(19)
+
+    if (/^[\w-]+\/[\w-.]+(\?.*)?$/.test(href2)) {
+      const username = /^([\w-]+)/.exec(href2)![1]
+      if (username && !noneUsers.has(username)) {
+        return prefix + href2.replace(/(^[\w-]+\/[\w-.]+).*/, "$1")
+      }
+    }
+  }
+
+  return undefined
+}
+
+function getTopicsUrl(href: string) {
+  if (href.startsWith(prefix)) {
+    const href2 = href.slice(19)
+
+    if (/^topics\/[\w-.]+(\?.*)?$/.test(href2)) {
+      return prefix + href2.replace(/(^topics\/[\w-.]+).*/, "$1")
+    }
+  }
+
+  return undefined
+}
 
 const site = {
   matches: /github\.com/,
@@ -35,33 +100,45 @@ const site = {
       (element: HTMLAnchorElement) => {
         const href = element.href
 
-        if (href.startsWith("https://github.com/")) {
+        if (href.startsWith(prefix)) {
           if (/since|until/.test(href)) {
             return false
           }
 
-          if (/^https:\/\/github\.com\/[\w-]+$/.test(href)) {
-            const username = /^https:\/\/github\.com\/([\w-]+)$/.exec(href)![1]
-            if (!noneUsers.has(username)) {
-              const meta = { type: "user" }
-              element.utags = { meta }
-              return true
-            }
-
-            return false
-          }
-
-          if (/(author%3A|author=)[\w-]+/.test(href)) {
-            const username = /(author%3A|author=)([\w-]+)/.exec(href)![2]
-            const key = `https://github.com/${username}`
+          let key = getUserProfileUrl(href)
+          if (key) {
+            const username = /^https:\/\/github\.com\/([\w-]+)$/.exec(key)![1]
             const title = username
             const meta = { title, type: "user" }
             element.utags = { key, meta }
             return true
           }
+
+          key = getRepoUrl(href)
+          if (key) {
+            const title = key.replace(prefix, "")
+            const meta = { title, type: "repo" }
+            element.utags = { key, meta }
+            return true
+          }
+
+          key = getTopicsUrl(href)
+          if (key) {
+            const text = element.textContent!.trim()
+            if (text === "#") {
+              return false
+            }
+
+            const title = key.replace(prefix, "")
+            const meta = { title, type: "topic" }
+            element.utags = { key, meta }
+            return true
+          }
+
+          return false
         }
 
-        return false
+        return true
       }
     ) as HTMLAnchorElement[]
   },
@@ -72,7 +149,15 @@ const site = {
     // Repo popup
     ".Popover-message .Link--secondary",
     ".IssueLabel",
+    ".subnav-links",
+    ".btn",
+    ".filter-item",
   ],
+  validMediaSelectors: [
+    // Repo icon
+    "svg.octicon-repo",
+  ],
+  getStyle: () => styleText,
 }
 
 export default site
