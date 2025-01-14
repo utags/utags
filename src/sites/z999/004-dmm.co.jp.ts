37ef6a4a6063d56ea4294ccf6a995b0325c6d014
pipecraft <pipecraft.net@gmail.com>
2025-01-15 02:58:19 +0800

    fix(dmm.co.jp): update style

--- src/sites/z999/004-dmm.co.jp.ts
+++ src/sites/z999/004-dmm.co.jp.ts
@@ -1,9 +1,42 @@
-import { $$, getAttribute } from "browser-extension-utils"
+import { $, $$, getAttribute } from "browser-extension-utils"
 import styleText from "data-text:./004-dmm.co.jp.scss"
 
 import defaultSite from "../default"
 
 const prefix = "https://www.dmm.co.jp/"
+
+function getCanonicalUrl(url: string) {
+  if (url.startsWith(prefix)) {
+    const href2 = url.slice(prefix.length)
+    if (href2.includes("/=/")) {
+      return prefix + href2.replace(/\?.*/, "")
+    }
+  }
+
+  return url
+}
+
+function getProductUrl(url: string) {
+  if (url.startsWith(prefix)) {
+    const href2 = url.slice(prefix.length)
+    if (href2.includes("/detail/=/cid=")) {
+      return prefix + href2.replace(/\?.*/, "")
+    }
+  }
+
+  return undefined
+}
+
+function getMakerUrl(url: string) {
+  if (url.startsWith(prefix)) {
+    const href2 = url.slice(prefix.length)
+    if (href2.includes("/list/=/article=maker/id=")) {
+      return prefix + href2.replace(/\?.*/, "")
+    }
+  }
+
+  return undefined
+}
 
 const site = {
   matches: /dmm\.co\.jp/,
@@ -11,11 +44,8 @@ const site = {
     return $$("a[href]:not(.utags_text_tag)").filter(
       (element: HTMLAnchorElement) => {
         const href = element.href
-        const hrefAttr = getAttribute(element, "href")
 
-        if (hrefAttr.startsWith("#")) {
-          return false
-        }
+        element.dataset.utags_position = "LB"
 
         if (!href.startsWith(prefix)) {
           return true
@@ -45,7 +75,45 @@ const site = {
     ".pageNationList",
     ".nav-text-container",
     ".sub-nav-link",
+    ".m-listHeader",
+    ".dcd-review__rating_map",
+    ".dcd-review_boxpagenation",
   ],
+  validMediaSelectors: [
+    ".mainList",
+    ".pickup .fn-responsiveImg",
+    "#l-areaRecommendProduct",
+  ],
+  addExtraMatchedNodes(matchedNodesSet: Set<HTMLElement>) {
+    let key = getProductUrl(location.href)
+    if (key) {
+      // post title
+      const element = $("h1.productTitle__txt")
+      if (element) {
+        const title = element.textContent!.trim()
+        if (title) {
+          const meta = { title }
+          element.utags = { key, meta }
+          matchedNodesSet.add(element)
+        }
+      }
+    }
+
+    key = getMakerUrl(location.href)
+    if (key) {
+      // post title
+      const element = $(".circleProfile__name span")
+      if (element) {
+        const title = element.textContent!.trim()
+        if (title) {
+          const meta = { title }
+          element.utags = { key, meta }
+          matchedNodesSet.add(element)
+        }
+      }
+    }
+  },
+  getCanonicalUrl,
   getStyle: () => styleText,
 }
 
