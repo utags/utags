7d63d7204d6a3e22ed302d78cfc1009573e572f0
pipecraft <pipecraft.net@gmail.com>
2025-01-15 14:53:48 +0800

    fix(xiaohongshu.com): update style

--- src/sites/z001/018-xiaohongshu.com.ts
+++ src/sites/z001/018-xiaohongshu.com.ts
@@ -1,8 +1,26 @@
 import { $, $$, hasClass } from "browser-extension-utils"
+import styleText from "data-text:./018-xiaohongshu.com.scss"
 
 import defaultSite from "../default"
 
 const prefix = "https://www.xiaohongshu.com/"
+
+function getCanonicalUrl(url: string) {
+  if (url.startsWith(prefix)) {
+    const href2 = url.slice(prefix.length)
+    // https://www.xiaohongshu.com/search_result?keyword=abcd&type=54&source=web_note_detail_r10
+    if (href2.startsWith("search_result") && href2.includes("keyword")) {
+      return (
+        prefix +
+        "search_result/?" +
+        href2.replace(/.*?(keyword=[^&]*).*/, "$1") +
+        "&type=54"
+      )
+    }
+  }
+
+  return url
+}
 
 function getUserProfileUrl(url: string, exact = false) {
   if (url.startsWith(prefix)) {
@@ -19,6 +37,7 @@ function getUserProfileUrl(url: string, exact = false) {
   return undefined
 }
 
+// 2024年开始小红书需要 xsec_token 参数才可以访问。目前先只打标签，无法访问的问题日后通过其他方式实现可访问。比如破解参数生成，或通过中转站。
 function getPostUrl(url: string) {
   if (url.startsWith(prefix)) {
     const href2 = url.slice(28)
@@ -44,28 +63,35 @@ const site = {
     return $$("a[href]:not(.utags_text_tag)").filter(
       (element: HTMLAnchorElement) => {
         const href = element.href
+        element.dataset.utags_position = "LB"
 
-        if (!href.includes("www.xiaohongshu.com")) {
+        if (!href.startsWith(prefix)) {
           return true
         }
 
         let key = getUserProfileUrl(href, true)
         if (key) {
-          const titleElement = hasClass(element, "name")
-            ? element
-            : $(".name", element)
+          if (element.closest(".note-content-user")) {
+            console.log(element)
+          }
+
+          const titleElement =
+            (hasClass(element, "name") ? element : $(".name", element)) ||
+            element
           let title: string | undefined
           if (titleElement) {
             title = titleElement.textContent!
           }
 
-          const meta = { type: "user" }
-          if (title) {
-            meta.title = title
-          } else {
+          if (!title) {
             return false
           }
 
+          if (element.closest(".author-container .author-wrapper .name")) {
+            // element.dataset.utags_position = "LT"
+          }
+
+          const meta = { type: "user", title }
           element.utags = { key, meta }
           element.dataset.utags = element.dataset.utags || ""
 
@@ -83,7 +109,14 @@ const site = {
       }
     ) as HTMLAnchorElement[]
   },
-  excludeSelectors: [...defaultSite.excludeSelectors, ".cover"],
+  excludeSelectors: [
+    ...defaultSite.excludeSelectors,
+    ".cover",
+    ".side-bar",
+    ".dropdown-nav",
+    ".dropdown-container",
+    ".interaction-info",
+  ],
   addExtraMatchedNodes(matchedNodesSet: Set<HTMLElement>) {
     let key = getUserProfileUrl(location.href)
     if (key) {
@@ -95,6 +128,7 @@ const site = {
           const meta = { title, type: "user" }
           element.utags = { key, meta }
           matchedNodesSet.add(element)
+          element.dataset.utags_position = "LT"
         }
       }
     }
@@ -113,6 +147,8 @@ const site = {
       }
     }
   },
+  getCanonicalUrl,
+  getStyle: () => styleText,
 }
 
 export default site
