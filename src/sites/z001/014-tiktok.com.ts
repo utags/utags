c5b6ddc81f9e85b438143e52e8291f6f70a09046
pipecraft <pipecraft.net@gmail.com>
2025-01-25 17:15:23 +0900

    fix(tiktok.com): update style, apply the special tag effects

--- src/sites/z001/014-tiktok.com.ts
+++ src/sites/z001/014-tiktok.com.ts
@@ -1,14 +1,19 @@
 import { $, $$ } from "browser-extension-utils"
+import styleText from "data-text:./014-tiktok.com.scss"
 
 import defaultSite from "../default"
 
 const prefix = "https://www.tiktok.com/"
 
-function getUserProfileUrl(url: string) {
+function getUserProfileUrl(url: string, exact = false) {
   if (url.startsWith(prefix)) {
     const href2 = url.slice(23)
-    if (/^@[\w.]+/.test(href2)) {
-      return prefix + href2.replace(/(^@[\w.]+).*/, "$1")
+    if (exact) {
+      if (/^@[\w.-]+([?#].*)?$/.test(href2)) {
+        return prefix + href2.replace(/(^@[\w.-]+).*/, "$1")
+      }
+    } else if (/^@[\w.-]+/.test(href2)) {
+      return prefix + href2.replace(/(^@[\w.-]+).*/, "$1")
     }
   }
 
@@ -17,30 +22,41 @@ function getUserProfileUrl(url: string) {
 
 const site = {
   matches: /tiktok\.com/,
+  listNodesSelectors: [
+    ".css-ulyotp-DivCommentContentContainer",
+    ".css-1gstnae-DivCommentItemWrapper",
+    ".css-x6y88p-DivItemContainerV2",
+  ],
+  conditionNodesSelectors: [
+    '.css-ulyotp-DivCommentContentContainer a[href^="/@"]',
+    '.css-1gstnae-DivCommentItemWrapper a[href^="/@"]',
+    '.css-x6y88p-DivItemContainerV2 a[href^="/@"]',
+  ],
   getMatchedNodes() {
     return $$("a[href]:not(.utags_text_tag)").filter(
       (element: HTMLAnchorElement) => {
         const href = element.href
-        if (href.startsWith(prefix)) {
-          const pathname = element.pathname
-          if (/^\/@[\w.]+$/.test(pathname)) {
-            const titleElement = $("h3", element)
-            let title: string | undefined
-            if (titleElement) {
-              title = titleElement.textContent!
-            }
+        if (!href.startsWith(prefix)) {
+          return true
+        }
 
-            const key = prefix + pathname.slice(1)
-            const meta = { type: "user" }
-            if (title) {
-              meta.title = title
-            }
+        const key = getUserProfileUrl(href, true)
+        if (key) {
+          const titleElement = $('h3,[data-e2e="browse-username"]', element)
+          const title = titleElement
+            ? titleElement.textContent!.trim()
+            : element.textContent!.trim()
 
-            element.utags = { key, meta }
-            element.dataset.utags = element.dataset.utags || ""
-
-            return true
+          if (!title) {
+            return false
           }
+
+          const meta = { type: "user", title }
+
+          element.utags = { key, meta }
+          // element.dataset.utags = element.dataset.utags || ""
+
+          return true
         }
 
         return false
@@ -52,6 +68,10 @@ const site = {
     ".avatar-anchor",
     '[data-e2e*="avatar"]',
     '[data-e2e="user-card-nickname"]',
+  ],
+  validMediaSelectors: [
+    '[data-e2e="browse-bluev"]',
+    '[data-e2e="recommend-card"]',
   ],
   addExtraMatchedNodes(matchedNodesSet: Set<HTMLElement>) {
     // profile header
@@ -66,6 +86,7 @@ const site = {
       }
     }
   },
+  getStyle: () => styleText,
 }
 
 export default site
