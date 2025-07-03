94638ea3e4dc3534148de931070a3d307ee9c52e
pipecraft <pipecraft.net@gmail.com>
2025-07-04 00:39:31 +0800

    0.14.1

--- build/userscript-prod/utags.user.js
+++ build/userscript-prod/utags.user.js
@@ -4,7 +4,7 @@
 // @namespace            https://utags.pipecraft.net/
 // @homepageURL          https://github.com/utags/utags#readme
 // @supportURL           https://github.com/utags/utags/issues
-// @version              0.14.0
+// @version              0.14.1
 // @description          Add custom tags or notes to links such as users, posts and videos. For example, tags can be added to users or posts on a forum, making it easy to identify them or block their posts and replies. It works on X (Twitter), Reddit, Facebook, Threads, Instagram, Youtube, TikTok, GitHub, Greasy Fork, Hacker News, pixiv and numerous other websites.
 // @description:zh-CN    这是个超实用的工具，能给用户、帖子、视频等链接添加自定义标签和备注信息。比如，可以给论坛的用户或帖子添加标签，易于识别他们或屏蔽他们的帖子和回复。支持 V2EX, X, Reddit, Greasy Fork, GitHub, B站, 抖音, 小红书, 知乎, 掘金, 豆瓣, 吾爱破解, pixiv, LINUX DO, 小众软件, NGA, BOSS直聘等网站。
 // @icon                 data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='%23ff6361' class='bi bi-tags-fill' viewBox='0 0 16 16'%3E %3Cpath d='M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z'/%3E %3Cpath d='M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z'/%3E %3C/svg%3E
@@ -1350,6 +1350,33 @@
     "en,en-US": en_default2,
     "zh,zh-CN": zh_cn_default2,
   })
+  var MIN_VALID_TIMESTAMP = 631152e6
+  var MAX_VALID_TIMESTAMP = 9999999999999
+  function isValidDate(date) {
+    return (
+      typeof date === "number" &&
+      date > MIN_VALID_TIMESTAMP &&
+      date < MAX_VALID_TIMESTAMP
+    )
+  }
+  function normalizeCreated(created, updated, defaultDate) {
+    const isCreatedValid = isValidDate(created)
+    const isUpdatedValid = isValidDate(updated)
+    const minValidDate = Math.min(
+      isCreatedValid ? created : Infinity,
+      isUpdatedValid ? updated : Infinity
+    )
+    return Number.isFinite(minValidDate) ? minValidDate : defaultDate
+  }
+  function normalizeUpdated(created, updated, defaultDate) {
+    const isCreatedValid = isValidDate(created)
+    const isUpdatedValid = isValidDate(updated)
+    const maxValidDate = Math.max(
+      isCreatedValid ? created : 0,
+      isUpdatedValid ? updated : 0
+    )
+    return maxValidDate || defaultDate
+  }
   function trimTitle(title) {
     if (!title) return ""
     return title.replaceAll(/\s+/gm, " ").trim()
@@ -1476,7 +1503,7 @@
   async function getEmojiTags() {
     return splitTags(getSettingsValue("emojiTags") || "")
   }
-  var currentExtensionVersion = "0.13.0"
+  var currentExtensionVersion = "0.14.1"
   var currentDatabaseVersion = 3
   var storageKey2 = "extension.utags.urlmap"
   var cachedUrlMap = {}
@@ -1534,7 +1561,7 @@
   }
   var getTags = getBookmark
   async function saveBookmark(key, tags, meta) {
-    var _a
+    var _a, _b
     const now = Date.now()
     const bookmarksStore = await getBookmarksStore()
     const urlMap = bookmarksStore.data
@@ -1556,8 +1583,11 @@
       const newMeta = __spreadProps(
         __spreadValues(__spreadValues({}, existingData.meta), meta),
         {
-          created:
-            ((_a = existingData.meta) == null ? void 0 : _a.created) || now,
+          created: normalizeCreated(
+            (_a = existingData.meta) == null ? void 0 : _a.created,
+            (_b = existingData.meta) == null ? void 0 : _b.updated,
+            now
+          ),
           updated: now,
         }
       )
@@ -1594,8 +1624,22 @@
     )
   }
   async function migrateV2toV3(bookmarksStore) {
-    var _a, _b
-    console.log("Starting migration from V2 to V3")
+    var _a, _b, _c
+    const meta = bookmarksStore.meta
+    if (meta.databaseVersion === 2) {
+      console.log("Starting migration database from V2 to V3")
+    } else if (
+      meta.databaseVersion === 3 &&
+      meta.extensionVersion === "0.13.0"
+    ) {
+      console.log(
+        "Starting migration extension from v0.13.0 to v" +
+          currentDatabaseVersion
+      )
+    } else {
+      console.log("No migration needed")
+      return
+    }
     const now = Date.now()
     let minCreated = now
     const bookmarksStoreNew = createEmptyBookmarksStore()
@@ -1666,16 +1710,26 @@
           delete bookmarkV2.meta.updated
         }
       }
-      const meta = __spreadProps(__spreadValues({}, bookmarkV2.meta), {
-        created: ((_a = bookmarkV2.meta) == null ? void 0 : _a.created) || now,
-        updated: ((_b = bookmarkV2.meta) == null ? void 0 : _b.updated) || now,
+      const normalizedCreated = normalizeCreated(
+        (_a = bookmarkV2.meta) == null ? void 0 : _a.created,
+        (_b = bookmarkV2.meta) == null ? void 0 : _b.updated,
+        now
+      )
+      const normalizedUpdated = normalizeUpdated(
+        normalizedCreated,
+        (_c = bookmarkV2.meta) == null ? void 0 : _c.updated,
+        now
+      )
+      const meta2 = __spreadProps(__spreadValues({}, bookmarkV2.meta), {
+        created: normalizedCreated,
+        updated: normalizedUpdated,
       })
       const bookmarkV3 = {
         tags: bookmarkV2.tags,
-        meta,
+        meta: meta2,
       }
       bookmarksStoreNew.data[key] = bookmarkV3
-      minCreated = Math.min(minCreated, meta.created)
+      minCreated = Math.min(minCreated, normalizedCreated)
     }
     bookmarksStoreNew.meta.created = minCreated
     await persistBookmarksStore(bookmarksStoreNew)
@@ -1712,7 +1766,10 @@
     if (!isVersionCompatible) {
       return
     }
-    if (meta.databaseVersion === 2) {
+    if (
+      meta.databaseVersion === 2 ||
+      (meta.databaseVersion === 3 && meta.extensionVersion === "0.13.0")
+    ) {
       await migrateV2toV3(bookmarksStore)
       await initBookmarksStore()
       return
