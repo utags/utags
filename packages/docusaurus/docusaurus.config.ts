263fe24968c6d50d45993c8b8c3586084aabc5cf
pipecraft <pipecraft.net@gmail.com>
2025-09-12 11:33:34 +0800

    docs: trailingSlash: true

--- packages/docusaurus/docusaurus.config.ts
+++ packages/docusaurus/docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'utags', // Usually your GitHub org/user name.
   projectName: 'utags', // Usually your repo name.
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
