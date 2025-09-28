e3743fbee306581885587d00bd62343f09983885
pipecraft <pipecraft.net@gmail.com>
2025-09-28 10:23:59 +0800

    fix: Vue framework issue side effect (#78)

--- packages/extension/src/sites/index.ts
+++ packages/extension/src/sites/index.ts
@@ -355,8 +355,8 @@ const addMatchedNodes = (matchedNodesSet: Set<UtagsHTMLElement>) => {
     const originalKey = element.href
     let utags = getElementUtags(element)
     // vue 等框架会重复利用 element 对象，只修改 href 属性。每次需要验证 href 值是否与缓存的 utags.originalKey 值一致
-    if (!utags || utags.originalKey !== originalKey) {
-      utags = { key: '', meta: {}, originalKey }
+    if (!utags || (utags.originalKey && utags.originalKey !== originalKey)) {
+      utags = { key: '', meta: {} }
     }
 
     const key = utags.key || getCanonicalUrl(originalKey)
@@ -377,6 +377,7 @@ const addMatchedNodes = (matchedNodesSet: Set<UtagsHTMLElement>) => {
     // Store utags data in WeakMap instead of directly on the element
     setElementUtags(element, {
       key,
+      originalKey,
       meta: utags.meta ? Object.assign(meta, utags.meta) : meta,
     })
 
