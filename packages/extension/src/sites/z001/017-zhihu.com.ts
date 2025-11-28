db8bc23299faf857cad184e0a4864ae43dcb27b2
pipecraft <pipecraft.net@gmail.com>
2025-11-29 01:14:13 +0800

    fix: zhihu.com

--- packages/extension/src/sites/z001/017-zhihu.com.ts
+++ packages/extension/src/sites/z001/017-zhihu.com.ts
@@ -1,14 +1,56 @@
 import { $, $$ } from 'browser-extension-utils'
+import styleText from 'data-text:./017-zhihu.com.scss'
 import { getTrimmedTitle } from 'utags-utils'
 
+import {
+  addVisited,
+  markElementWhetherVisited,
+  setVisitedAvailable,
+} from '../../modules/visited'
 import { setUtags } from '../../utils/dom-utils'
 import defaultSite from '../default'
 
 export default (() => {
   const prefix = 'https://www.zhihu.com/'
+  const prefix2 = 'https://zhuanlan.zhihu.com/'
+
+  function getCanonicalUrl(url: string) {
+    // url = deleteUrlParameters(url, '*')
+
+    if (url.startsWith(prefix)) {
+      const href2 = getUserProfileUrl(url, true)
+      if (href2) {
+        return href2
+      }
+    }
+
+    if (url.startsWith(prefix2)) {
+      const href2 = getPostUrl(url, true)
+      if (href2) {
+        return href2
+      }
+    }
+
+    return url
+  }
+
+  function getPostUrl(url: string, exact = false) {
+    if (url.startsWith(prefix2)) {
+      const href2 = url.slice(prefix2.length)
+      if (exact) {
+        if (/^p\/\d+$/.test(href2)) {
+          return prefix2 + href2.replace(/^(p\/\d+).*/, '$1')
+        }
+      } else if (/^p\/\d+/.test(href2)) {
+        return prefix2 + href2.replace(/^(p\/\d+).*/, '$1')
+      }
+    }
+
+    return undefined
+  }
 
   function getUserProfileUrl(url: string, exact = false) {
-    if (url.startsWith(prefix)) {
+    if (url.startsWith(prefix + 'people/')) {
       const href2 = url.slice(22)
       if (exact) {
         if (/^people\/[\w-]+(\?.*)?$/.test(href2)) {
@@ -35,22 +77,52 @@ export default (() => {
         return true
       }
 
-      if (href.startsWith(prefix + 'people/')) {
-        const key = getUserProfileUrl(href, true)
-        if (key) {
-          const titleElement = $('.name', element)
-          let title: string | undefined
-          if (titleElement) {
-            title = titleElement.textContent!
+      let key = getUserProfileUrl(href, true)
+      if (key) {
+        const titleElement = $('.name', element)
+        let title: string | undefined
+        if (titleElement) {
+          title = titleElement.textContent!
+        }
+
+        const meta: Record<string, any> = { type: 'user' }
+        if (title) {
+          meta.title = title
+        }
+
+        setUtags(element, key, meta)
+        // element.dataset.utags = element.dataset.utags || ""
+
+        return true
+      }
+
+      key = getPostUrl(href)
+      if (key) {
+        const titleElement = $('h1.PostItem-Title,.LinkCard-title', element)
+        const title = getTrimmedTitle(titleElement || element)
+        if (!title) {
+          return false
+        }
+
+        const meta = { type: 'post', title }
+        setUtags(element, key, meta)
+        markElementWhetherVisited(key, element)
+        element.dataset.utags = element.dataset.utags || ''
+
+        return true
+      }
+
+      if (href.startsWith(prefix)) {
+        const href2 = href.slice(prefix.length)
+        if (/^(question|collection|column)\//.test(href2)) {
+          const titleElement = $('h1.PostItem-Title,.LinkCard-title', element)
+          const title = getTrimmedTitle(titleElement || element)
+          if (!title) {
+            return false
           }
 
-          const meta = { type: 'user' }
-          if (title) {
-            meta.title = title
-          }
-
-          setUtags(element, key, meta)
-          // element.dataset.utags = element.dataset.utags || ""
+          const meta = { title }
+          setUtags(element, '', meta)
 
           return true
         }
@@ -63,9 +135,31 @@ export default (() => {
       '.NumberBoard',
       '.ProfileMain-tabs',
       '.Profile-lightList',
+      '.QuestionMainAction',
+      '.ContentItem-time',
+    ],
+    validMediaSelectors: [
+      // post card
+      'a.LinkCard',
     ],
     addExtraMatchedNodes(matchedNodesSet: Set<HTMLElement>) {
-      const key = getUserProfileUrl(location.href)
+      let key = getPostUrl(location.href)
+      if (key) {
+        addVisited(key)
+        const element = $('h1.Post-Title')
+        if (element) {
+          const title = getTrimmedTitle(element)
+          if (title) {
+            const meta = { title, type: 'post' }
+            setUtags(element, key, meta)
+            element.dataset.utags_node_type = 'link'
+            matchedNodesSet.add(element)
+            markElementWhetherVisited(key, element)
+          }
+        }
+      }
+
+      key = getUserProfileUrl(location.href)
       if (key) {
         // profile header
         const element = $('h1.ProfileHeader-title .ProfileHeader-name')
@@ -79,5 +173,7 @@ export default (() => {
         }
       }
     },
+    getStyle: () => styleText,
+    getCanonicalUrl,
   }
 })()
