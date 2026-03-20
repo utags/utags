91925140eede798fa90350bcd86058e78b6c4d85
pipecraft <pipecraft.net@gmail.com>
2026-03-20 19:57:29 +0800

    feat: Add firefox addon mv3 version

--- packages/extension/scripts/firefox/update-manifest.mjs
+++ packages/extension/scripts/firefox/update-manifest.mjs
@@ -14,3 +14,20 @@ if (manifest.content_scripts) {
 }
 
 fs.writeFileSync(filePath, JSON.stringify(manifest))
+
+{
+  const filePath = "build/firefox-mv3-prod/manifest.json"
+  const manifest = JSON.parse(fs.readFileSync(filePath, "utf8"))
+
+  delete manifest.web_accessible_resources
+
+  if (manifest.content_scripts) {
+    for (const script of manifest.content_scripts) {
+      if (script.css && script.css.length === 0) {
+        delete script.css
+      }
+    }
+  }
+
+  fs.writeFileSync(filePath, JSON.stringify(manifest))
+}
